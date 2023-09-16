202213b0a0e83f9355d734eb55b35bccab6a552d
Pierre <pierremarais67@gmail.com>
2023-09-16 17:19:17 +0200

    Reorder functions for better readability

--- src/main/kotlin/pierremarais/plugins/RateLimiter.kt
+++ src/main/kotlin/pierremarais/plugins/RateLimiter.kt
@@ -9,6 +9,24 @@ interface RateLimiter {
 }
 
 class TokenBucket(private val bucketSize: Int = 10, private val tokenRefillRate: Long = 1000) : RateLimiter {
+    internal val buckets = mutableMapOf<IPAddress, Int>()
+    override fun processRequest(requestIp: IPAddress) {
+        buckets.merge(requestIp, bucketSize - 1) { tokens, _ ->
+            if (tokens > 0) {
+                tokens - 1
+            } else {
+                throw Exception("Out of tokens!")
+            }
+        }
+    }
+    internal fun refreshTokens() {
+        buckets.entries.removeIf { it.value + 1 > bucketSize }
+        buckets.forEach { entry ->
+            val ip = entry.key
+            buckets.merge(ip, bucketSize) { t, _ -> t + 1}
+        }
+    }
+
     internal var refresherThread: Thread? = null
     fun startRefreshingTokensThread(): Thread =
         if (refresherThread == null) {
@@ -23,22 +41,4 @@ class TokenBucket(private val bucketSize: Int = 10, private val tokenRefillRate:
         } else
             refresherThread!!
 
-    internal val buckets = mutableMapOf<IPAddress, Int>()
-    internal fun refreshTokens() {
-        buckets.entries.removeIf { it.value + 1 > bucketSize }
-        buckets.forEach { entry ->
-            val ip = entry.key
-            buckets.merge(ip, bucketSize) { t, _ -> t + 1}
-        }
-    }
-
-    override fun processRequest(requestIp: IPAddress) {
-        buckets.merge(requestIp, bucketSize - 1) { tokens, _ ->
-            if (tokens > 0) {
-                tokens - 1
-            } else {
-                throw Exception("Out of tokens!")
-            }
-        }
-    }
 }
